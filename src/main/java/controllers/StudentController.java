87793485213a4a48e6005cf118de512cd4893c9e
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-09-04 17:37:31 -0500

    Merge remote-tracking branch 'origin/sprint-1' into sprint-1
    # Conflicts:
    #	src/main/java/controllers/StudentController.java

--- src/main/java/controllers/StudentController.java
+++ src/main/java/controllers/StudentController.java
@@ -1,6 +1,9 @@
 package controllers;
 
 public class StudentController {
+    public void execute(){
+        System.out.println("Hello World");
+    }
     public void destroy(){
         System.out.println("Destroy");
     }
