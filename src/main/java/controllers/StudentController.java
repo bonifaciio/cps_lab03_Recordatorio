6ad01f5dff797264cb048f98a54bc2578186ad0c
Rodrigo Salva <rodrigodanielsalvasaccatoma@gmail.com>
2025-09-04 17:15:57 -0500

    Primeros cambios en el package entity y repository con clase  con init

--- src/main/java/controllers/StudentController.java
+++ src/main/java/controllers/StudentController.java
@@ -3,6 +3,12 @@ package controllers;
 public class StudentController {
 
     public void init(){
-        System.out.printf("Se holamunde");
+        System.out.printf(" hOLA MUNDO");
+    }
+    public void init2(){
+        System.out.printf(" hOLA MUNDO");
+    }
+    public void init3(){
+        System.out.printf(" hOLA MUNDO");
     }
 }
