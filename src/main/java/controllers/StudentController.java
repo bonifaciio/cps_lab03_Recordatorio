00c841a092e3c08d2f8210e2036944ae5c0a56f6
Rodrigo Salva <rodrigodanielsalvasaccatoma@gmail.com>
2025-09-04 17:22:24 -0500

    Primeros cambios en el package entity y repository con clase  con init

--- src/main/java/controllers/StudentController.java
+++ src/main/java/controllers/StudentController.java
@@ -2,7 +2,14 @@ package controllers;
 
 public class StudentController {
 
+    public void init(){
+        System.out.printf("Se agrego el metodo init");
+    }
+
     public void execute(){
         System.out.printf("Te ejecute");
+    }
+    public void destroy(){
+        System.out.printf("Se agrego el metodo destroy");
     }
 }
