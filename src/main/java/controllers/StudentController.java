5e64fe14e341742ccd85970ba6bd0ecd61a9c13f
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2025-09-04 17:19:29 -0500

    se agrego StudentService

--- src/main/java/controllers/StudentController.java
+++ src/main/java/controllers/StudentController.java
@@ -2,7 +2,7 @@ package controllers;
 
 public class StudentController {
 
-    public void init(){
-        System.out.printf("hola mai ");
+    public void execute(){
+        System.out.printf("Te ejecute");
     }
 }
